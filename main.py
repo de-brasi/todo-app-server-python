66f3b14f26d86bbfb8893e519b5d39b27395874f
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-02 11:47:01 +0300

    can be terminated by http request and got some useless data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import threading
 
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
@@ -6,27 +7,44 @@ from http.server import HTTPServer
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print('new request!')
-        print('path is:', self.path)
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
+        if self.path.startswith("/terminate"):
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
 
-        # TODO: routing
-        response_data = {"key": "value", "int key": 123}
-        response_data_json = json.dumps(response_data)
-        self.wfile.write(response_data_json.encode())
+            self.wfile.write("Server shutting down...".encode())
+
+            threading.Thread(target=self.shutdown_server).start()
+        else:
+            print('Request path is:', self.path)
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+
+            response_data = {
+                "example key": "example value",
+                "example int key": 123
+            }
+            response_data_json = json.dumps(response_data)
+            self.wfile.write(response_data_json.encode())
+
+    def shutdown_server(self):
+        self.server.shutdown()
 
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
+    server_address = ('127.0.0.1', 8000)
+    print('starts on:', server_address[0] + ':' + str(server_address[1]))
     httpd = server_class(server_address, handler_class)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
+        pass
+    finally:
         httpd.server_close()
-    except ...:
-        print("Server stopped with unhandled error!")
 
 
 if __name__ == '__main__':
