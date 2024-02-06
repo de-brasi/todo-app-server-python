17ce9b5687849f192182951b26c96b8a31db869b
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-06 17:00:43 +0300

    add field "data_schema" for entity data-class

--- infrastructure/db/entities.py
+++ infrastructure/db/entities.py
@@ -1,9 +1,13 @@
+from typing import List
 from dataclasses import dataclass
+
+from infrastructure.db.storage.field_description import FieldDescription
 
 
 @dataclass
 class TodoTaskCSVEntity:
     data: str
+    data_schema: List[FieldDescription]
 
     def get_data(self):
         return self.data
