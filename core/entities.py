5e3c599af7f343b832c1b3cdd3cb63737876c5bc
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-06 14:30:30 +0300

    erase todo

--- core/entities.py
+++ core/entities.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class TodoTask:
-    task_id: int         # todo: как организовать выбор уникальных id для новых задач в проекте?
+    task_id: int
     description: str
     group: str
     important: bool
