930fef9795352ab6950c6ac84d859ea5564e20c1
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-06 14:06:47 +0300

    add todo

--- core/entities.py
+++ core/entities.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class TodoTask:
-    id: int         # todo: как организовать выбор уникальных id для новых задач в проекте?
+    task_id: int         # todo: как организовать выбор уникальных id для новых задач в проекте?
     description: str
     group: str
     important: bool
