71ee79fa20f9cdc0d487d09b40591c3326c78a11
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-07 14:18:37 +0300

    rework to class MainService

--- core/services.py
+++ core/services.py
@@ -4,54 +4,56 @@ from core.entities import TodoTask
 from core.repositories import TasksRepository
 
 
-def task_entity_to_dict_mapper(arg: TodoTask) -> dict:
-    res = {
-        'task_id': arg.task_id,
-        'description': arg.description,
-        'group': arg.group,
-        'important': arg.important,
-        'done': arg.done
-    }
-    return res
+class MainService:
+    def __init__(self, storage: TasksRepository):
+        self.storage = storage
 
+    @staticmethod
+    def task_entity_to_dict_mapper(arg: TodoTask) -> dict:
+        res = {
+            'task_id': arg.task_id,
+            'description': arg.description,
+            'group': arg.group,
+            'important': arg.important,
+            'done': arg.done
+        }
+        return res
 
-def task_entity_to_todo_task_mapper(arg: dict) -> TodoTask:
-    return TodoTask(
-        task_id=arg['task_id'],
-        description=arg['description'],
-        group=arg['group'],
-        important=arg['important'],
-        done=arg['done']
-    )
+    @staticmethod
+    def task_entity_to_todo_task_mapper(arg: dict) -> TodoTask:
+        return TodoTask(
+            task_id=arg['task_id'],
+            description=arg['description'],
+            group=arg['group'],
+            important=arg['important'],
+            done=arg['done']
+        )
 
-
-# TODO: нужен класс чтобы в конструкторе передать реализацию интерфейса TasksRepository
-def get_tasks_from_db(storage: TasksRepository) -> str:
-    tasks = storage.get_tasks()
-    print("got tasks from db:", tasks)
-    tasks_as_dicts = []
-    for todo in tasks:
-        tasks_as_dicts.append(task_entity_to_dict_mapper(todo))
-
-    result = json.dumps(tasks_as_dicts)
-    return result
-
-
-def save_tasks_to_db(storage: TasksRepository, to_save: str):
-    try:
-        tasks = json.loads(to_save)
-
-        tasks_as_entity = []
+    def get_tasks_from_db(self) -> str:
+        tasks = self.storage.get_tasks()
+        print("got tasks from db:", tasks)
+        tasks_as_dicts = []
         for todo in tasks:
-            assert isinstance(todo, dict)
-            tasks_as_entity.append(task_entity_to_todo_task_mapper(todo))
+            tasks_as_dicts.append(MainService.task_entity_to_dict_mapper(todo))
 
-        print("got tasks from client:", tasks_as_entity)
+        result = json.dumps(tasks_as_dicts)
+        return result
 
-        storage.save_tasks(tasks_as_entity)
-    except json.JSONDecodeError as e:
-        print("decoding error when decode string:", to_save)
-        raise RuntimeError("internal error")
-    except AssertionError:
-        print("assertion error when decode string:", to_save, "; expected encoded list od dicts")
-        raise RuntimeError("internal error")
+    def save_tasks_to_db(self, to_save: str):
+        try:
+            tasks = json.loads(to_save)
+
+            tasks_as_entity = []
+            for todo in tasks:
+                assert isinstance(todo, dict)
+                tasks_as_entity.append(MainService.task_entity_to_todo_task_mapper(todo))
+
+            print("got tasks from client:", tasks_as_entity)
+
+            self.storage.save_tasks(tasks_as_entity)
+        except json.JSONDecodeError as e:
+            print("decoding error when decode string:", to_save)
+            raise RuntimeError("internal error")
+        except AssertionError:
+            print("assertion error when decode string:", to_save, "; expected encoded list od dicts")
+            raise RuntimeError("internal error")
